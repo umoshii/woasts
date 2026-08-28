520edaa2f296e4746cb42b6d2842bb484def63e0
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-08-29 00:49:53 +0200

    feat: FontDescription.DEFAULT

--- src/main/kotlin/io/github/umoshii/woasts/utils/FontUtils.kt
+++ src/main/kotlin/io/github/umoshii/woasts/utils/FontUtils.kt
@@ -5,11 +5,10 @@ import net.minecraft.network.chat.Component
 import net.minecraft.network.chat.FontDescription
 import net.minecraft.network.chat.MutableComponent
 import net.minecraft.network.chat.Style
-import net.minecraft.resources.Identifier
 
 object FontUtils {
-    val iconFont    = Style.EMPTY.withFont(FontDescription.Resource(Woasts.id("icons")))
-    val defaultFont = Style.EMPTY.withFont(FontDescription.Resource(Identifier.fromNamespaceAndPath("minecraft", "default")))
+    val iconFont =    Style.EMPTY.withFont(FontDescription.Resource(Woasts.id("icons")))
+    val defaultFont = Style.EMPTY.withFont(FontDescription.DEFAULT)
 
     enum class Icons(val component: MutableComponent) {
         PING("\uE000"),
@@ -18,6 +17,6 @@ object FontUtils {
         BIOME("\uE003"),
         ;
 
-        constructor(c: String) :  this(Component.literal(c).withStyle(iconFont))
+        constructor(c: String) : this(Component.literal(c).withStyle(iconFont))
     }
 }
