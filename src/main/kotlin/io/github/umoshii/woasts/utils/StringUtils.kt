2e127720de4d145d4242ed6892cc5cc03111549b
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-09-01 16:15:03 +0200

    credits

--- src/main/kotlin/io/github/umoshii/woasts/utils/StringUtils.kt
+++ src/main/kotlin/io/github/umoshii/woasts/utils/StringUtils.kt
@@ -3,7 +3,7 @@ package io.github.umoshii.woasts.utils
 import java.text.NumberFormat
 
 /*
-    String.toTitleCase() implementation taken from @SkyblockAPI/SkyblockAPI
+    String.toTitleCase(), Number.toFormattedString() implementations taken from @SkyblockAPI/SkyblockAPI
     https://github.com/SkyblockAPI/SkyblockAPI/blob/b5143eff1782ba6046b9e0a46803d2895dbc70f6/src/main/kotlin/tech/thatgravyboat/skyblockapi/utils/extentions/StringExtensions.kt#L192-L193
 
     Licensed under MIT License, see CREDITS.md for more
